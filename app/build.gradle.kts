c1aba1cc606841b6cc6d47950f9ce063da18e1e8
akamfjsjskqkdckpapapsf-design <akamfjsjskqkdckpapapsf@gmail.com>
2026-08-12 16:19:09 +0300

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,23 +2,33 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
+
 android {
-    namespace = "com.example.ime.latin"
+    namespace = "com.example.customkeyboard"
     compileSdk = 34
+
     defaultConfig {
-        applicationId = "com.example.ime.latin"
+        applicationId = "com.example.customkeyboard"
         minSdk = 21
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
     }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions { jvmTarget = "17" }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
+
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
