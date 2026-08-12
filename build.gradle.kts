02fd20f451bf2e2552a54568185cda25c340243b
akamfjsjskqkdckpapapsf-design <akamfjsjskqkdckpapapsf@gmail.com>
2026-08-12 16:16:16 +0300

    .

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,38 @@
 plugins {
-    id("com.android.application") version "8.1.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.customkeyboard"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.customkeyboard"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
 }
