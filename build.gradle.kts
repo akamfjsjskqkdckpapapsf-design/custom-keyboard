dac0910fab410b75a04d82d80fb0ccc29a569473
akamfjsjskqkdckpapapsf-design <akamfjsjskqkdckpapapsf@gmail.com>
2026-08-12 14:50:34 +0300

    .

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,3 @@ plugins {
     id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
