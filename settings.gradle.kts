54162b684ed6a99b6cc42edc8bb416c00c606557
akamfjsjskqkdckpapapsf-design <akamfjsjskqkdckpapapsf@gmail.com>
2026-08-16 22:11:18 +0300

    .

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,11 +5,17 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        maven { url "https://jitpack.io" }
+        maven {
+            url = java.net.URI("https://jitpack.io")
+        }
     }
 }
+
+rootProject.name = "custom-keyboard"
+include(":app")
